dd10d4c2b7a4f678fc9c782dd54b1589ee1f1ee2
Frederic Cambus <fcambus@users.sourceforge.net>
2015-06-27 00:41:08 +0200

    Changing hits by countries output format as well

--- src/output.c
+++ src/output.c
@@ -19,26 +19,27 @@
 void output(Results results) {
 	json_t *jsonObject = json_object();
 	json_t *hitsObject = json_object();
-	json_t *countriesObject = json_object();
+	json_t *countriesArray = json_array();
 	json_t *hoursArray = json_array();
 			
 	for (int loop=0; loop<255; loop++) {
 		if (results.countries[loop] != 0) {
-			json_object_set_new(countriesObject, GeoIP_code_by_id(loop), json_integer(results.countries[loop]));
+			json_array_append_new(countriesArray, json_pack("{s:s, s:i}", "data", GeoIP_code_by_id(loop), "hits", results.countries[loop]));
 		}
 	}
 
 	for (int loop=0; loop<24; loop++) {
 		if (results.hours[loop] != 0) {
-			json_array_append_new(hoursArray, json_pack("{s:i, s:i}", "data", loop, "hits", results.hours[loop]));			
+			json_array_append_new(hoursArray, json_pack("{s:i, s:i}", "data", loop, "hits", results.hours[loop]));
 		}
 	}
 
 	json_object_set_new(hitsObject, "ipv4", json_integer(results.hitsIPv4));
 	json_object_set_new(hitsObject, "ipv6", json_integer(results.hitsIPv6));
 	json_object_set_new(hitsObject, "total", json_integer(results.hits));
-	json_object_set_new(hitsObject, "countries", countriesObject);
+	json_object_set_new(hitsObject, "countries", countriesArray);
 	json_object_set_new(hitsObject, "hours", hoursArray);
+
 	json_object_set_new(jsonObject, "date", json_string(results.timeStamp));
 	json_object_set_new(jsonObject, "file_size", json_integer(results.fileSize));
 	json_object_set_new(jsonObject, "processed_lines", json_integer(results.processedLines));
