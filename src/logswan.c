5e5ea5aa0e8470c7e5835ab347c711291a349e18
Frederic Cambus <fcambus@users.sourceforge.net>
2015-07-26 13:40:54 +0200

    Add a newline character after printing Logswan version

--- src/logswan.c
+++ src/logswan.c
@@ -79,7 +79,7 @@ int main (int argc, char *argv[]) {
 	while ((getoptFlag = getopt(argc, argv, "v")) != -1) {
 		switch(getoptFlag) {
 		case 'v':
-			printf(VERSION);
+			printf("%s\n", VERSION);
 			return 0;
 		}
 	}
