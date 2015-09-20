bb3107bfc32c04be5772eed5e04acad380eee295
Frederic Cambus <fcambus@users.sourceforge.net>
2015-09-20 10:33:44 +0200

    Raising LINE_MAX_LENGTH limit

--- src/logswan.h
+++ src/logswan.h
@@ -4,7 +4,7 @@
 /* https://github.com/fcambus/logswan                                        */
 /*                                                                           */
 /* Created:      2015/05/31                                                  */
-/* Last Updated: 2015/07/19                                                  */
+/* Last Updated: 2015/09/20                                                  */
 /*                                                                           */
 /* Logswan is released under the BSD 3-Clause license.                       */
 /* See LICENSE file for details.                                             */
@@ -15,6 +15,6 @@
 #define LOGSWAN_H
 
 #define VERSION "Logswan"
-#define LINE_MAX_LENGTH 4096
+#define LINE_MAX_LENGTH 65536
 
 #endif /* LOGSWAN_H */
