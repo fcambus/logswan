e603bd71fcc1786a5a80ae24febd1a3df8acc55f
Frederic Cambus <fcambus@users.sourceforge.net>
2015-11-02 15:56:47 +0100

    Logswan 1.02 release

--- src/logswan.h
+++ src/logswan.h
@@ -1,10 +1,10 @@
 /*****************************************************************************/
 /*                                                                           */
-/* Logswan 1.01 (c) by Frederic Cambus 2015                                  */
+/* Logswan 1.02 (c) by Frederic Cambus 2015                                  */
 /* https://github.com/fcambus/logswan                                        */
 /*                                                                           */
 /* Created:      2015/05/31                                                  */
-/* Last Updated: 2015/10/01                                                  */
+/* Last Updated: 2015/11/02                                                  */
 /*                                                                           */
 /* Logswan is released under the BSD 3-Clause license.                       */
 /* See LICENSE file for details.                                             */
@@ -14,7 +14,7 @@
 #ifndef LOGSWAN_H
 #define LOGSWAN_H
 
-#define VERSION "Logswan 1.01"
+#define VERSION "Logswan 1.02"
 #define LINE_MAX_LENGTH 65536
 
 #endif /* LOGSWAN_H */
--- src/parse.h
+++ src/parse.h
@@ -1,10 +1,10 @@
 /*****************************************************************************/
 /*                                                                           */
-/* Logswan 1.01 (c) by Frederic Cambus 2015                                  */
+/* Logswan 1.02 (c) by Frederic Cambus 2015                                  */
 /* https://github.com/fcambus/logswan                                        */
 /*                                                                           */
 /* Created:      2015/05/31                                                  */
-/* Last Updated: 2015/10/01                                                  */
+/* Last Updated: 2015/11/02                                                  */
 /*                                                                           */
 /* Logswan is released under the BSD 3-Clause license.                       */
 /* See LICENSE file for details.                                             */
