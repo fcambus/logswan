072aa1ed4be9c4f4977755a2016704d65dd234df
Frederic Cambus <fred@statdns.com>
2017-01-09 00:46:44 +0100

    Simplify internal JSON array and object names

--- src/output.c
+++ src/output.c
@@ -19,20 +19,20 @@
 #include "output.h"
 
 char *output(struct results* results) {
-	json_t *jsonObject = json_object();
-	json_t *hitsObject = json_object();
-	json_t *visitsObject = json_object();
-	json_t *continentsArray = json_array();
-	json_t *countriesArray = json_array();
-	json_t *hoursArray = json_array();
-	json_t *httpStatusArray = json_array();
-	json_t *methodsArray = json_array();
-	json_t *protocolsArray = json_array();
+	json_t *output = json_object();
+	json_t *hits = json_object();
+	json_t *visits = json_object();
+	json_t *continents = json_array();
+	json_t *countries = json_array();
+	json_t *hours = json_array();
+	json_t *status = json_array();
+	json_t *methods = json_array();
+	json_t *protocols = json_array();
 
 	for (size_t loop=0; loop<CONTINENTS; loop++) {
 		if (results->continents[loop]) {
 			json_array_append_new(
-			    continentsArray,
+			    continents,
 			    json_pack("{s:s, s:s, s:i}",
 			    "data", continentsId[loop],
 			    "name", continentsNames[loop],
@@ -43,7 +43,7 @@ char *output(struct results* results) {
 	for (size_t loop=0; loop<COUNTRIES; loop++) {
 		if (results->countries[loop]) {
 			json_array_append_new(
-			    countriesArray,
+			    countries,
 			    json_pack("{s:s, s:s, s:i}",
 			    "data", GeoIP_code_by_id(loop),
 			    "name", GeoIP_name_by_id(loop),
@@ -54,7 +54,7 @@ char *output(struct results* results) {
 	for (size_t loop=0; loop<24; loop++) {
 		if (results->hours[loop]) {
 			json_array_append_new(
-			    hoursArray,
+			    hours,
 			    json_pack("{s:i, s:i}",
 			    "data", loop,
 			    "hits", results->hours[loop]));
@@ -64,7 +64,7 @@ char *output(struct results* results) {
 	for (size_t loop=0; loop<STATUS_CODE_MAX; loop++) {
 		if (results->status[loop]) {
 			json_array_append_new(
-			    httpStatusArray,
+			    status,
 			    json_pack("{s:i, s:i}",
 			    "data", loop,
 			    "hits", results->status[loop]));
@@ -74,7 +74,7 @@ char *output(struct results* results) {
 	for (size_t loop=0; loop<METHODS; loop++) {
 		if (results->methods[loop]) {
 			json_array_append_new(
-			    methodsArray,
+			    methods,
 			    json_pack("{s:s, s:i}",
 			    "data", methodsNames[loop],
 			    "hits", results->methods[loop]));
@@ -84,37 +84,37 @@ char *output(struct results* results) {
 	for (size_t loop=0; loop<PROTOCOLS; loop++) {
 		if (results->protocols[loop]) {
 			json_array_append_new(
-			    protocolsArray,
+			    protocols,
 			    json_pack("{s:s, s:i}",
 			    "data", protocolsNames[loop],
 			    "hits", results->protocols[loop]));
 		}
 	}
 
-	json_object_set_new(hitsObject, "ipv4", json_integer(results->hitsIPv4));
-	json_object_set_new(hitsObject, "ipv6", json_integer(results->hitsIPv6));
-	json_object_set_new(hitsObject, "total", json_integer(results->hits));
+	json_object_set_new(hits, "ipv4", json_integer(results->hitsIPv4));
+	json_object_set_new(hits, "ipv6", json_integer(results->hitsIPv6));
+	json_object_set_new(hits, "total", json_integer(results->hits));
 
-	json_object_set_new(visitsObject, "ipv4", json_integer(results->visitsIPv4));
-	json_object_set_new(visitsObject, "ipv6", json_integer(results->visitsIPv6));
-	json_object_set_new(visitsObject, "total", json_integer(results->visits));
+	json_object_set_new(visits, "ipv4", json_integer(results->visitsIPv4));
+	json_object_set_new(visits, "ipv6", json_integer(results->visitsIPv6));
+	json_object_set_new(visits, "total", json_integer(results->visits));
 
-	json_object_set_new(jsonObject, "date", json_string(results->timeStamp));
-	json_object_set_new(jsonObject, "generator", json_string(VERSION));
-	json_object_set_new(jsonObject, "file_name", json_string(results->fileName));
-	json_object_set_new(jsonObject, "file_size", json_integer(results->fileSize));
-	json_object_set_new(jsonObject, "processed_lines", json_integer(results->processedLines));
-	json_object_set_new(jsonObject, "invalid_lines", json_integer(results->invalidLines));
-	json_object_set_new(jsonObject, "bandwidth", json_integer(results->bandwidth));
-	json_object_set_new(jsonObject, "runtime", json_real(results->runtime));
-	json_object_set_new(jsonObject, "hits", hitsObject);
-	json_object_set_new(jsonObject, "visits", visitsObject);
-	json_object_set_new(jsonObject, "continents", continentsArray);
-	json_object_set_new(jsonObject, "countries", countriesArray);
-	json_object_set_new(jsonObject, "hours", hoursArray);
-	json_object_set_new(jsonObject, "methods", methodsArray);
-	json_object_set_new(jsonObject, "protocols", protocolsArray);
-	json_object_set_new(jsonObject, "status", httpStatusArray);
+	json_object_set_new(output, "date", json_string(results->timeStamp));
+	json_object_set_new(output, "generator", json_string(VERSION));
+	json_object_set_new(output, "file_name", json_string(results->fileName));
+	json_object_set_new(output, "file_size", json_integer(results->fileSize));
+	json_object_set_new(output, "processed_lines", json_integer(results->processedLines));
+	json_object_set_new(output, "invalid_lines", json_integer(results->invalidLines));
+	json_object_set_new(output, "bandwidth", json_integer(results->bandwidth));
+	json_object_set_new(output, "runtime", json_real(results->runtime));
+	json_object_set_new(output, "hits", hits);
+	json_object_set_new(output, "visits", visits);
+	json_object_set_new(output, "continents", continents);
+	json_object_set_new(output, "countries", countries);
+	json_object_set_new(output, "hours", hours);
+	json_object_set_new(output, "methods", methods);
+	json_object_set_new(output, "protocols", protocols);
+	json_object_set_new(output, "status", status);
 
-	return json_dumps(jsonObject, JSON_INDENT(3) | JSON_PRESERVE_ORDER);
+	return json_dumps(output, JSON_INDENT(3) | JSON_PRESERVE_ORDER);
 }
