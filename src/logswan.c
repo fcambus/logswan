109f6379a067e9e8e6dc03b571cee3e4ee853f77
Frederic Cambus <fred@statdns.com>
2018-06-27 23:09:30 +0200

    Define _POSIX_C_SOURCE 199309L, fixes 'clock_gettime’ implicit declaration on Linux

--- src/logswan.c
+++ src/logswan.c
@@ -13,6 +13,7 @@
 /*****************************************************************************/
 
 #define _XOPEN_SOURCE 600
+#define _POSIX_C_SOURCE 199309L
 #define _POSIX_SOURCE
 
 #include <arpa/inet.h>
