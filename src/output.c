8e3aa64e07fe35ab2369e004d1c9f5cb69364f60
Frederic Cambus <fred@statdns.com>
2019-01-19 14:35:16 +0100

    Use lighter file headers everywhere.

--- src/output.c
+++ src/output.c
@@ -1,16 +1,14 @@
-/*****************************************************************************/
-/*                                                                           */
-/* Logswan 2.0.3                                                             */
-/* Copyright (c) 2015-2019, Frederic Cambus                                  */
-/* https://www.logswan.org                                                   */
-/*                                                                           */
-/* Created:      2015-05-31                                                  */
-/* Last Updated: 2018-10-15                                                  */
-/*                                                                           */
-/* Logswan is released under the BSD 2-Clause license.                       */
-/* See LICENSE file for details.                                             */
-/*                                                                           */
-/*****************************************************************************/
+/*
+ * Logswan 2.0.3
+ * Copyright (c) 2015-2019, Frederic Cambus
+ * https://www.logswan.org
+ *
+ * Created:      2015-05-31
+ * Last Updated: 2019-01-19
+ *
+ * Logswan is released under the BSD 2-Clause license.
+ * See LICENSE file for details.
+ */
 
 #include <stdint.h>
 
