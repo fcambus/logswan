9eb00b97c35261d4bcd2f0a6ec66c0ac2cc4d196
Frederic Cambus <fred@statdns.com>
2020-05-23 11:25:05 +0200

    Use CLOCK_REALTIME if CLOCK_MONOTONIC is not available.

--- compat/compat.h
+++ compat/compat.h
@@ -9,6 +9,11 @@
 #include "strtonum.h"
 #endif
 
+/* Use CLOCK_REALTIME if CLOCK_MONOTONIC is not available */
+#ifndef CLOCK_MONOTONIC
+#define CLOCK_MONOTONIC CLOCK_REALTIME
+#endif
+
 #ifndef timespecsub
 #define	timespecsub(tsp, usp, vsp)					\
 	do {								\
