645c2f0ccec38a168c5e0d2fa6428f6b650b3e0b
Frederic Cambus <fred@statdns.com>
2020-07-03 16:34:52 +0200

    Logswan 2.1.6 release.

--- src/config.h
+++ src/config.h
@@ -1,5 +1,5 @@
 /*
- * Logswan 2.1.5
+ * Logswan 2.1.6
  * Copyright (c) 2015-2020, Frederic Cambus
  * https://www.logswan.org
  *
@@ -13,7 +13,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define VERSION "Logswan 2.1.5"
+#define VERSION "Logswan 2.1.6"
 
 enum {
 	HLL_BITS = 20,
