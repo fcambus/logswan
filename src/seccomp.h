e672f19d4c11ac7d1dbeaeabb711116e1733c075
Frederic Cambus <fred@statdns.com>
2020-09-17 09:53:11 +0200

    Add support for seccomp on ARM.

--- src/seccomp.h
+++ src/seccomp.h
@@ -25,6 +25,8 @@
 #define SECCOMP_AUDIT_ARCH AUDIT_ARCH_I386
 #elif defined(__x86_64__)
 #define SECCOMP_AUDIT_ARCH AUDIT_ARCH_X86_64
+#elif defined(__arm__)
+#define SECCOMP_AUDIT_ARCH AUDIT_ARCH_ARM
 #elif defined(__aarch64__)
 #define SECCOMP_AUDIT_ARCH AUDIT_ARCH_AARCH64
 #else
